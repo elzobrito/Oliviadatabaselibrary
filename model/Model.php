c8ffd468cb2fedcf54be7e0bb33a7daa41c23e72
Elzo Brito dos Santos Filho <elzo.santos@cps.sp.gov.br>
2021-10-29 12:27:32 -0300

    atualização

---
+++ model/Model.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace OliviaDatabaseLibrary\Model;
+
+use OliviaDatabaseLibrary\Config\Database;
+use OliviaDatabaseLibrary\QueryBuilder;
+use OliviaDatabaseLibrary\Model as iModel;
+
+class Model implements iModel
+{
+    protected $table;
+    protected $drive;
+    protected $fillable = array();
+
+    /**
+     * @var array
+     */
+    private $clausules = [];
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return $this
+     */
+    function __call($name, $arguments)
+    {
+        $clausule = $arguments[0];
+        if (count($arguments) > 1) {
+            $clausule = $arguments;
+        }
+        $this->clausules[strtolower($name)] = $clausule;
+
+        return $this;
+    }
+
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    public function update($fields, $wheres, $values)
+    {
+        $qb = new QueryBuilder(Database::getDB($this->drive));
+        return  $qb
+            ->table($this->table)
+            ->fields($fields)
+            ->where($wheres)
+            ->update($values);
+    }
+
+    public function save($fields = null, $valores = null)
+    {
+        $qb = new QueryBuilder(Database::getDB($this->drive));
+        return $qb
+            ->table($this->table)
+            ->fields(($fields ?? $this->fill()))
+            ->insert(($valores ?? $this->values()));
+    }
+
+    public function delete($id = null, $primaryKey = null)
+    {
+        if ($id || isset($this->id)) {
+            $value = $id ?? isset($this->id);
+            $qb = new QueryBuilder(Database::getDB($this->drive));
+            return $qb
+                ->table($this->table)
+                ->where([($primaryKey ?? 'id') . ' = ?'])
+                ->delete([$value]);
+        }
+    }
+
+    public function find($fields, $wheres, $values, $join = null, $group = null, $order = null, $having = null, $limit = null)
+    {
+        $qb = new QueryBuilder(Database::getDB($this->drive));
+        return $qb->table($this->table)
+            ->fields($fields ?? ['*'])
+            ->where($wheres)
+            ->join($join)
+            ->group($group)
+            ->order($order)
+            ->having($having)
+            ->limit($limit)
+            ->select($values);
+    }
+
+
+    public function all($fields = null, $order = null, $limit = null)
+    {
+        $qb = new QueryBuilder(Database::getDB($this->drive));
+        return $qb->table($this->table)
+            ->fields($fields ?? ['*'])
+            ->order($order)
+            ->limit($limit)
+            ->select();
+    }
+
+    public function count($wheres = null, $values = null, $join = null, $group = null, $order = null, $having = null, $limit = null)
+    {
+        $qb = new QueryBuilder(Database::getDB($this->drive));
+        return $qb->table($this->table)
+            ->fields(['count(*) as total'])
+            ->where($wheres)
+            ->join($join)
+            ->group($group)
+            ->order($order)
+            ->having($having)
+            ->limit($limit)
+            ->select($values);
+    }
+
+    public function findForId($id, $primaryKey = null, $fields = null, $join = null, $order = null, $limit = null)
+    {
+        $qb = new QueryBuilder(Database::getDB($this->drive));
+        return $qb->table($this->table)
+            ->fields($fields ?? ['*'])
+            ->where([($primaryKey ?? 'id') . ' = ?'])
+            ->join($join)
+            ->order($order)
+            ->limit($limit)
+            ->select([$id]);
+    }
+
+    public function values()
+    {
+        $filds = [];
+        $array = [];
+        $filds = $this->fill();
+        foreach ($this as $key => $value) {
+            if (in_array($key, $filds))
+                $array[] = $value;
+        }
+        return $array;
+    }
+
+
+    public function request_cripto()
+    {
+        $val_temp = null;
+        foreach ($this->fillable as $key => $value)
+            if (substr($value, 0, 2) == 'id') {
+                if (isset($_REQUEST[$value]))
+                    if (!is_numeric($_REQUEST[$value])) {
+                        $val_temp = $this->decryptIt($_REQUEST[$value]);
+                        if (is_numeric($val_temp)) {
+                            $this->$value = $val_temp;
+                        } else {
+                            $this->$value = $_REQUEST[$value];
+                        }
+                    } else {
+                        $this->$value = $_REQUEST[$value];
+                    }
+            } else {
+                $this->$value = $_REQUEST[$value];
+            }
+    }
+
+    public function request($fields = null)
+    {
+        $campos = $fields ?? $this->fillable;
+        foreach ($campos as $key => $value)
+            if (isset($_REQUEST[$value]))
+                $this->$value = $_REQUEST[$value];
+    }
+
+    public function fill()
+    {
+        $array = [];
+        foreach ($this->fillable as $key => $value) {
+            $array[] = $value;
+        }
+        return $array;
+    }
+}
---
+++ src/Connection.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace OliviaDatabaseLibrary;
+
+use \PDO;
+use \PDOStatement;
+use \Exception;
+use \PDOException;
+
+/**
+ * Class Connection
+ * @package elzobrito
+ */
+class Connection
+{
+    /**
+     * @var PDO
+     */
+    private $pdo = null;
+    /**
+     * @var array
+     */
+    private $options = [];
+
+    /**
+     * Connection constructor.
+     * @param array $options
+     * @throws Exception
+     */
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return PDO
+     */
+    protected function connect()
+    {
+        if (!$this->pdo) {
+            try {
+                $options = array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "UTF8"',
+                    // PDO::MYSQL_ATTR_SSL_CA => __DIR__ . DIRECTORY_SEPARATOR . 'BaltimoreCyberTrustRoot.crt.pem'
+                );
+                $this->pdo = new PDO($this->dsn(), $this->options['user'], $this->options['password'], $options);
+                // set the PDO error mode to exception
+                // $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                $this->status = "Falha ao tentar conectar: \n" . $e->getMessage();
+            }
+        }
+        return $this->pdo;
+    }
+
+    /**
+     * @return string
+     */
+    protected function dsn()
+    {
+        return "{$this->options['driver']}:host={$this->options['host']};port={$this->options['port']};dbname={$this->options['database']}";
+    }
+
+    /**
+     * @param $sql
+     * @return PDOStatement
+     */
+    private final function statement($sql)
+    {
+        try {
+            return $this->connect()->prepare($sql);
+        } catch (PDOException $e) {
+            $this->status = "Falha ao tentar conectar: \n" . $e->getMessage();
+        }
+    }
+
+    /**
+     * @param string $sql
+     * @param array $values
+     * @return string
+     */
+    protected final function executeInsert($sql, array $values)
+    {
+        $statement = $this->statement($sql);
+
+        if ($statement && $statement->execute(array_values($values))) {
+            return $this->connect()->lastInsertId();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $values
+     * @return array
+     */
+    protected final function executeSelect($sql, array $values)
+    {
+        $statement = $this->statement($sql);
+
+        if ($statement && $statement->execute(array_values($values))) {
+            return $statement->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $values
+     * @return int
+     */
+    protected final function executeUpdate($sql, array $values)
+    {
+        return $this->execute($sql, $values);
+    }
+
+    /**
+     * @param string $sql
+     * @param array $values
+     * @return int
+     */
+    protected final function executeDelete($sql, array $values)
+    {
+        return $this->execute($sql, $values);
+    }
+
+    /**
+     * @param $sql
+     * @param array $values
+     * @return int|null
+     */
+    protected final function execute($sql, array $values)
+    {
+        $statement = $this->statement($sql);
+
+        if ($statement && $statement->execute(array_values($values))) {
+            return $statement->rowCount();
+        }
+
+        return null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+}
